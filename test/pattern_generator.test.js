4d220546513c117233c683ad136d78870d66b0b9
Abdul Redd <abdul@turing.edu>
2023-12-14 00:58:06 -0500

    initial commit

---
+++ test/pattern_generator.test.js
@@ -0,0 +1,61 @@
+const { expect } = require("chai");
+const PatternGenerator = require("../lib/pattern_generator");
+
+describe("PatternGenerator", () => {
+  let pg;
+
+  beforeEach(() => {
+    pg = new PatternGenerator();
+  });
+
+  describe.skip("Part 1: Verify Pattern Compliance", () => {
+    describe(".verify", () => {
+      it("Given a generated serial number, return true or false based on whether or not the input matches the pattern", () => {
+        const pattern = ".#.@";
+
+        expect(pg.verify("A3B1", pattern)).to.be.true;
+        expect(pg.verify("AABA", pattern)).to.be.false;
+      });
+    });
+  });
+
+  describe.skip("Part 2: Generate The Nth Value In the Pattern", () => {
+    describe(".generate", () => {
+      it("Given a specific pattern, assuming that incrementing happens from right to left (like normal numbers), generate the Nth value in the sequence", () => {
+        const pattern = "@.#.";
+
+        expect(pg.generate(0, pattern)).to.equal("0A0A");
+        expect(pg.generate(27, pattern)).to.equal("0A1B");
+        expect(pg.generate(60, pattern)).to.equal("0A2I");
+        expect(pg.generate(999, pattern)).to.equal("0D8L");
+      });
+    });
+  });
+
+  describe.skip("Part 3: Possibilities", () => {
+    describe(".total_available", () => {
+      it("Given a specific pattern, determine how many total numbers are in the set", () => {
+        let pattern = ".#.";
+        expect(pg.total_available(pattern)).to.equal(6760);
+
+        pattern = "@.#.";
+        expect(pg.total_available(pattern)).to.equal(243360);
+
+        pattern = "";
+        expect(pg.total_available(pattern)).to.equal(0);
+      });
+    });
+  });
+
+  describe.skip("Part 4: Deduction", () => {
+    describe(".find_pattern_for", () => {
+      it("Given a sample of three serial numbers, output a pattern that could generate them. Prefer . and # over @ if they fit the sample serial numbers", () => {
+        const inputs = ["0A1B", "5C2Z", "9R9B"];
+        expect(pg.find_pattern_for(inputs)).to.equal("#.#.");
+
+        const inputs2 = ["0A1B", "5C2Z", "AR9B"];
+        expect(pg.find_pattern_for(inputs2)).to.equal("@.#.");
+      });
+    });
+  });
+});
